614d1c4a606db0e3fe10f7a2f82bd4169a5a2fa9
Dominik Košař <dom707@seznam.cz>
2024-03-05 16:33:03 +0100

    Přidání funkcí plus poznámky k animaci

--- Vyvojaky/Animation.cs
+++ Vyvojaky/Animation.cs
@@ -24,5 +24,21 @@ namespace Vyvojaky
             Output,
             Start
         }
+
+        public void ImagesForBlocks(Types orders) 
+        { 
+            //foreach ((int, Types) i in Panel) 
+            //{ 
+                
+            //}
+            //zjištění které bloky jsou propojeny a následně přiřadit příslušný tmavý obrázek
+        }
+
+        public async void Delays(int pause)
+        { 
+            //asynchronní metoda pro pauzu
+            //potřebná asynchronní funkce pro run možná
+            //po vyhodnocení bloků by se bloky vrátili do původní podoby
+        }
     }
 }
