117f54a5137a84d2a3f638111bfeb05b0b84a494
Dominik Košař <dom707@seznam.cz>
2024-03-05 16:00:54 +0100

    Třída animation pro animaci bloků

---
+++ Vyvojaky/Animation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vyvojaky
+{
+    internal class Animation
+    {   
+
+        public Animation() 
+        { 
+            
+        }
+
+        public enum Types 
+        { 
+            Var, 
+            Con, 
+            Switch,
+            Cycle,
+            Process, 
+            Output,
+            Start
+        }
+    }
+}
